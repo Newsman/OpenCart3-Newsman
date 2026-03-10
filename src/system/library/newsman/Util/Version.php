ebf0513e9e29b3d10783ae0c9db85fe2b3ea974a
Daniel Ifrim <daniel.ifrim7@gmail.com>
2026-03-10 17:00:03 +0200

    Fix API get version of integration/extension

--- src/system/library/newsman/Util/Version.php
+++ src/system/library/newsman/Util/Version.php
@@ -3,18 +3,12 @@
 namespace Newsman\Util;
 
 class Version extends \Newsman\Nzmbase {
+	const VERSION = '3.0.0';
+
 	/**
 	 * @return string
 	 */
 	public function getVersion() {
-		$composer_path = dirname(__DIR__, 5) . '/composer.json';
-		if (file_exists($composer_path)) {
-			$composer = json_decode(file_get_contents($composer_path), true);
-			if (isset($composer['version'])) {
-				return $composer['version'];
-			}
-		}
-
-		return '0.0.0';
+		return self::VERSION;
 	}
 }
